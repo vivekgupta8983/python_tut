ed9ee4d62a0c1acc5619af60f9955ac3638e4759
Vivek Gupta <vivek.gupta8983@gmail.com>
2019-08-05 20:30:57 +0530

    This is the Tuple example

---
+++ tuples.py
@@ -0,0 +1,14 @@
+"""
+Tuple
+it like a List but immutable
+Mean you cannot change them
+Tuples Does not support items Assignments
+"""
+
+mytuple = (1, 2, 3, 4, 5, 6)
+
+print(mytuple)
+print(mytuple[0])
+print(mytuple[1:])
+print(mytuple.index(4))
+print(mytuple.count(3))
