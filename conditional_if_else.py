c288054735f64dc187fb6686101a0c27c1cf4963
vivekgupta8983 <vivek.gupta@gmail.com>
2019-08-06 11:59:12 +0530

    This is condition Logic example for python tutorials

---
+++ conditional_if_else.py
@@ -0,0 +1,21 @@
+"""
+Conditional Logic
+"""
+
+
+value = 'green'
+
+if value == 'green':
+    print("Green")
+else:
+    print("Stop")
+
+
+
+    value= "red"
+if value == 'green':
+    print("Go")
+elif value == 'yellow':
+    print("prepare to stop")
+else:
+    print("Stop")
---
+++ while_loops.py
@@ -0,0 +1,20 @@
+"""
+Excute statements repeatedly
+Conditions are used to stop the execution of loops
+Iterable items are strings, lists, yuple, dictionary.
+"""
+"""
+x = 0
+while x < 10:
+    print("Value of x is : " +str(x))
+    x = x + 1
+"""
+
+l = []
+num = 0
+while num < 10:
+    l.append(num)
+    print("Value of num is: " + str(num))
+    num += 1
+
+print(l)
