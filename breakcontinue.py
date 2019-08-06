9e67a12ed0ce1532696ea8f0d4cf2037a8d1bc4e
vivekgupta8983 <vivek.gupta@gmail.com>
2019-08-06 12:13:42 +0530

    This is break and continue while loop example for python tutorials

---
+++ breakcontinue.py
@@ -0,0 +1,37 @@
+"""
+Break: To Break out of the closest enclosing loop
+Continue: Go to the start of closest enclosing loop
+"""
+"""
+#x = 0
+#while x < 10:
+#    print("Value of x is: " + str(x))
+#    x = x + 1
+
+    if x == 8:
+        break
+    print("This Example is Awesome")
+    print("*#"*20)
+"""
+"""
+x = 0
+while x < 10:
+    print("Value of x is: " + str(x))
+    x = x + 1
+
+    if x == 8:
+        continue
+    print("This Example is Awesome")
+    print("*#"*20)
+"""
+x = 0
+while x < 10:
+    print("Value of x is: " + str(x))
+    x = x + 1
+
+    if x == 8:
+        break
+    print("This Example is Awesome")
+    print("*#"*20)
+else:
+    print("Just Broke out of the Loop")
