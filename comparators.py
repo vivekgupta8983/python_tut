fa5a54963ffc0aaa9e161d7bc4a68cce4df73b12
vivekgupta8983 <vivek.gupta@gmail.com>
2019-08-06 10:42:15 +0530

    This is compaprators example for python tutorials

---
+++ comparators.py
@@ -0,0 +1,19 @@
+"""
+==  --> Value Equal to
+!=  --> not equal to
+<   --> Less Than
+>   --> Greater Than
+<=  --> less than equal to
+>=  --> Greater Than Equal to
+"""
+
+bool_one = 10 == 11
+not_equal = 10 != 10
+print(bool_one)
+print (not_equal)
+less_than = 10 < 11
+print(less_than)
+great_than = 10 > 9
+print(great_than)
+lt_eq = 10 <= 10
+print(lt_eq)
