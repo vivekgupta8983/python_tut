8d1c7d1ad6b5e24c9bd9672e57617a94f8729fdd
Vivek Gupta <vivek.gupta8983@gmail.com>
2019-08-08 19:57:07 +0530

    This is the example of read input() in python scripting

---
+++ Python_Script/Read_Input.py
@@ -0,0 +1,13 @@
+"""
+functions in Python that you can used to read data from the user:
+input()
+you can store the results from them into a variable.
+input() is used to read input from the user.
+"""
+
+name = input("What is your Name?")
+brithday = input("What is your Birthday?")
+age = input("How old is you?")
+
+print("%s was born on %s" % (name, brithday))
+print("your age is %s " % (age))
